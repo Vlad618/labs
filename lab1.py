4439704b20b808fde83fc5e88a11e96cd62fa8a5
Vlad618 <vladlopyp@gmail.com>
2025-09-04 09:25:52 +0300

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,28 @@
+age=16
+height=176.2
+name="Vlad"
+student=True
+student_in_the_group=26
+grades=[4, 5, 5, 4]
+digits=(1, 2, 3, 4, 5, 6, 7, 8, 9)
+numbers={10, 20, 30, 40, 50}
+print('age:', age, type(age))
+print('height:', height, type(height))
+print('name:', name, type(name))
+print('student:', student, type(student))
+print('student_in_the_group:', student_in_the_group, type(student_in_the_group))
+print('grades:', grades, type(grades))
+print('digits:', digits, type(digits))
+print('numbers:', numbers, type(numbers))
+
+j=10
+k=6
+
+a=j+k # додає числа
+b=j-k # віднімає числа
+c=j*k # множить числа
+d=j/k # ділить числа
+e=j%k # показує остачу від ділення
+f=j**k # підносить перше число до степеня другого
+g=j//k # бере тільки цілу частину від ділення
+print(a, b, c, d, e, f, g)
