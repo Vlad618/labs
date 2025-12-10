82e4976e22fd8bb23f318042f2300d6cad785054
Vlad618 <vladlopyp@gmail.com>
2025-12-10 12:12:25 +0200

    lab6.py

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -4,5 +4,6 @@ from decorator import require_args
 def test(a, b, c=0):
     return a + b + c
 
-print(test(2,5, 7))      # OK
+print(test(2,5,7))      # OK
+
 print(test(2,3))            # Недостатньо аргументів
